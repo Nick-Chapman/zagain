8495ed142b1ce8e8cb3d215d0337d404331f27de
Nick Chapman <nic@amadido.co.uk>
2022-05-21 19:52:19 +0100

    line wrap and game-text prompt

--- src/Console.hs
+++ src/Console.hs
@@ -2,6 +2,7 @@
 module Console (runAction) where
 
 import Action (Action(..))
+import Control.Monad (when)
 import Control.Monad.Trans.Class (lift)
 import Text.Printf (printf)
 import qualified System.Console.ANSI as AN
@@ -20,6 +21,7 @@ runAction action = do
 replay :: [String] -> Action -> HL.InputT IO ()
 replay = loop 1
   where
+    showOld = False
     loop :: Int -> [String] -> Action -> HL.InputT IO ()
     loop n = \case
       line:rest -> inner
@@ -28,7 +30,9 @@ replay = loop 1
             Stop{} -> do pure ()
             TraceInstruction _ _ _ _ _ next -> do inner next
             Debug _ next -> do inner next
-            Output _ next -> do inner next
+            Output text next -> do
+              when showOld $ (lift $ putStr (col AN.Cyan text))
+              inner next
             Input _ f -> do
               lift $ printf "[%i] %s\n" n line
               loop (n+1) rest (f line)
@@ -38,25 +42,47 @@ replay = loop 1
 repl :: Int -> Action -> HL.InputT IO ()
 repl = loop []
   where
+    doWrap = True
     loop :: [String] -> Int -> Action -> HL.InputT IO ()
     loop buf n = \case
       Stop{} -> do pure ()
       TraceInstruction _ _ _ _ _ next -> do loop buf n next
       Debug _ next -> do loop buf n next
       Output text next -> do loop (text:buf) n next
-      Input count f -> do
-        lift $ flushBuffer count buf
-        HL.getInputLine (col AN.Green $ printf "[%i]" n) >>= \case
+      Input _count f -> do
+        let (text,prompt) = splitFinalPrompt (concat (reverse buf))
+        let wrap = if doWrap then lineWrap else \_ -> id
+        lift $ putStr (col AN.Cyan (wrap 80 text))
+        let xprompt = (col AN.Green $ printf "[%i]" n) ++ col AN.Cyan prompt
+        HL.getInputLine xprompt >>= \case
           Nothing -> pure () -- Ctr-D
           Just line -> do
             HL.modifyHistory (HL.addHistory line)
             HL.getHistory >>= lift . writeTranscript
             loop [] (n+1) (f line)
 
-    flushBuffer :: Int -> [String] -> IO ()
-    flushBuffer _count buf = do
-      -- printf "\n[executed: %d instructions]\n" count
-      mapM_ (putStr . col AN.Cyan) (reverse buf)
+splitFinalPrompt :: String -> (String,String)
+splitFinalPrompt s = do
+  let (xs,ys) = span (not . (== '\n')) (reverse s)
+  (reverse ys, reverse xs)
+
+lineWrap :: Int -> String -> String
+lineWrap max text =
+  unlines [ lineWrapOneLine (words line) | line <- lines text ]
+  where
+    lineWrapOneLine = \case
+      [] -> []
+      w1:ws -> loop [w1] 0 ws
+
+    loop :: [String] -> Int-> [String] -> String
+    loop acc pos = \case
+      [] -> concat (reverse acc)
+      w:ws -> do
+        let n = length w
+        let makeSplit =  pos + n + 1 > max
+        let pos' = if makeSplit then n else pos + n + 1
+        let sep = if makeSplit then "\n" else " "
+        loop (w : sep : acc) pos' ws
 
 
 col :: AN.Color -> String -> String
