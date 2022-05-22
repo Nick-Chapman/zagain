cb6bfb0d92177d389ac1226fcfce559234b02d12
Nick Chapman <nic@amadido.co.uk>
2022-05-22 12:17:38 +0100

    fix indentation during word-wrap in console

--- src/Console.hs
+++ src/Console.hs
@@ -79,21 +79,26 @@ splitFinalPrompt s = do
 
 lineWrap :: Int -> String -> String
 lineWrap max text =
-  unlines [ lineWrapOneLine (words line) | line <- lines text ]
+  unlines [ lineWrapOneLine line | line <- lines text ]
   where
-    lineWrapOneLine = \case
-      [] -> []
-      w1:ws -> loop [w1] (length w1) ws
-
-    loop :: [String] -> Int-> [String] -> String
-    loop acc pos = \case
-      [] -> concat (reverse acc)
-      w:ws -> do
-        let n = length w
-        let makeSplit =  pos + n + 1 > max
-        let pos' = if makeSplit then n else pos + n + 1
-        let sep = if makeSplit then "\n" else " "
-        loop (w : sep : acc) pos' ws
+    lineWrapOneLine line = do
+      let (wsIndent,remainder) = span (== ' ') line
+      case words remainder of
+        [] -> []
+        w1:ws -> do
+          let indentedW1 = wsIndent ++ w1
+          loop [indentedW1] (length indentedW1) ws
+          where
+            loop :: [String] -> Int-> [String] -> String
+            loop acc pos = \case
+              [] -> concat (reverse acc)
+              w:ws -> do
+                let indentedW = wsIndent ++ w
+                let posW = pos + length w + 1
+                let makeSplit = posW > max
+                let pos' = if makeSplit then length indentedW else posW
+                let sep = if makeSplit then "\n"++wsIndent else " "
+                loop (w : sep : acc) pos' ws
 
 
 col :: AN.Color -> String -> String
